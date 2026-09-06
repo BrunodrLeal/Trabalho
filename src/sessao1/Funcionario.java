3bdc6f0f788248d08e3849d2bfd246b4ad2a7013
Bruno dos Reis Leal <bdrleal@gmail.com>
2026-09-06 13:12:00 -0300

    Ajuste dos contructors dos cargos criados anteriormente.

--- src/sessao1/Funcionario.java
+++ src/sessao1/Funcionario.java
@@ -1,9 +1,11 @@
 package sessao1;
 
+import java.time.LocalDate;
+
 //Classe Abstrata - Classe Pai
 abstract class Funcionario {
 
-    // Atributos basicos
+    // Atributos básicos de um funcionario. 
     private int id;
     private String nome;
     private String cpf;
@@ -11,11 +13,11 @@ abstract class Funcionario {
     private String email;
     private String cargo;
     private double salario;
-    private String dataAdmissao;
+    private LocalDate dataAdmissao;
     private boolean ativo;
 
-    // constructor
-    public Funcionario(int id, String nome, String cpf, String telefone, String email, String cargo, double salario, String dataAdmicao, boolean ativo){
+    // constructor com todas as funções para as classes filhas
+    public Funcionario(int id, String nome, String cpf, String telefone, String email, String cargo, double salario, LocalDate dataAdmicao, boolean ativo){
         this.id = id;
         this.nome = nome;
         this.cpf = cpf;
@@ -27,79 +29,124 @@ abstract class Funcionario {
         this.ativo = ativo;
     }
 
-    // Setters para verificação
+    // Verificação se o id é válido
     public void setId (int id) {
         if(id > 0 ) {
             this.id = id;
-        } 
+        } else {
+            throw new IllegalArgumentException("ID inválido: " + id);
+        }
     }
+    // Verificação se o nome é válido
     public void setNome (String nome){
         if(nome != null && !nome.isEmpty()&& nome.length() >= 3){
             this.nome = nome;
         } else {
-            System.out.println("Nome inválido.");
+            throw new IllegalArgumentException("Nome inválido: "+ nome);
         }
     }
+    // Verificação se o cpf é válido
     public void setCpf (String cpf) {
-
+        if (cpf != null && cpf.replaceAll("\\D", "").length() == 11){
+            this.cpf = cpf;
+        } else {
+            throw new IllegalArgumentException("CPF inválido!"+ cpf);
+        }
     }
+    // Verificação se o telefone é válido
     public void setTelefone (String telefone) {
-
+        if(telefone != null && telefone.replaceAll("\\D", "").length() >= 10){
+            this.telefone = telefone;
+        } else {
+            System.out.println("Telefone inválido: "+ telefone);
+        }
     }
+    // Verificação se o email é válido
     public void setEmail (String email) {
-
+        if(email != null && email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$")){
+            this.email = email;
+        } else {
+            throw new IllegalArgumentException("E-mail inválido!"+ email);
+        }
     }
+    // Verificação se o cargo é válido
     public void setCargo (String cargo) {
-
+        if(cargo != null && !cargo.isEmpty()&& cargo.length() >= 3){
+            this.cargo = cargo;
+        } else {
+            throw new IllegalArgumentException("Cargo inválido: "+ cargo);
+        }
     }
+    // Verificação se o salário é válido
     public void setSalario(double salario) {
         if(salario >= 1){
             this.salario = salario;
         } else {
-            System.out.println("Salário inválido.");
+            throw new IllegalArgumentException("Sal´[ario inválido: "+ salario);
         }
     }
+    // Verificação se a data de admição é válida
     public void setDataAdmicao (String dataAdmicao) {
-
-    } 
-    public void setAtivo(boolean ativo){
-
+        if (dataAdmissao != null && !dataAdmissao.isAfter(LocalDate.now())) {
+        this.dataAdmissao = dataAdmissao;
+        } else {
+        throw new IllegalArgumentException(
+            "Data de admissão inválida: " + dataAdmissao
+        );
     }
-    // Getters para retorno
+    } 
+    // Verificação se o funcionario está ativo
+    public void setAtivo(boolean ativo){
+        if(ativo = true){
+            this.ativo = ativo;
+        } else{
+            throw new IllegalArgumentException("Funcionario desligado: " + ativo);
+        }
+    }
+    // Retorno id
     public int getId() {
         return id;
-        }
+    }
+    // Retorno Nome
     public String getNome(){
         return nome;
     }
+    // Retorno cpf
     public String getCpf() {
         return cpf;
     }
+    // Retorno Telefone
     public String getTelefone(){
         return telefone;
     }
+    // Retorno Email
     public String getEmail() {
         return email;
     }
+    // Retorno Cargo
     public String getCargo() {
         return cargo;
     }
+    // Retorno Salario
     public double getSalario() {
         return salario;
     }
-    public String getDataAdmicao() {
+    // Retorno Data de admição
+    public LocalDate getDataAdmicao() {
         return dataAdmissao;
     }
+    // Retorno Ativo
     public boolean getAtivo() {
         return ativo;
     }
-    
-    public abstract double calcularsalario();
-        
+    // Classe abstrat para classes filhas aplicarem.
+    public abstract double calcularSalario();
+
+    // Método de calculo de salário para classes filhas. 
     public void aumentarSalario(double valor) {
         if(valor > 0) {
-            salarioBase += valor;
-            System.out.println("Salário ajustado: R$ " + salarioBase);
+            salario += valor;
+            System.out.println("Salário ajustado: R$ " + salario);
         } else {
             System.out.println("Aumento invalidado.");
         }
--- src/sessao1/Gerente.java
+++ src/sessao1/Gerente.java
@@ -1,14 +1,16 @@
 package sessao1;
 
+import java.time.LocalDate;
+
 abstract  class Gerente extends Funcionario{
     
-    public Gerente(String nome, int cpf, double salarioBase){
-        super(nome, cpf, salarioBase);
+    public Gerente(int id, String nome, String cpf, String telefone, String email, String cargo, double salario, LocalDate dataAdmicao, boolean ativo){
+        super(id,nome,cpf,telefone,email,cargo,salario,dataAdmicao,ativo);
     }
 
     @Override
-    public double calcularsalario(){
-        return getSalarioBase() * 1.20;
+    public double calcularSalario(){
+        return getSalario() * 1.20;
     }
 
 }
